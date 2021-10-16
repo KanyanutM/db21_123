3b183c67e847360a7b5410920095a09b48189316
KanyanutM <91516461+KanyanutM@users.noreply.github.com>
2021-10-16 23:49:24 +0700

    Update people.php

---
+++ model/people.php
@@ -0,0 +1,48 @@
+<?php
+class People{
+    public $id_card;
+    public $Name;
+    public $lastname;
+    public $Address;
+    public $county;
+    public $Province;
+    public $Phone;
+
+    public function __construct($id_card,$Name,$lastname,$Address,$county,$Province,$Phone)
+    {
+        $this->id_card = $id_card;
+        $this->Name = $Name;
+        $this->lastname = $lastname;
+        $this->Address = $Address;
+        $this->county = $county;
+        $this->Province = $Province;
+        $this->Phone = $Phone;
+
+    }
+
+    public static function getAll()
+    {
+        $peopleList = [];
+        require("connection_connect.php");
+        $sql = "select * from People" ;
+        $result= $conn->query($sql);
+        while($my_row = $result->fetch_assoc())
+        {
+            $id_card = $my_row[id_card];
+            $Name = $my_row[Name];
+            $lastname = $my_row[lastname];
+            $Address = $my_row[Address];
+            $county = $my_row[county];
+            $Province = $my_row[Province];
+            $Phone = $my_row[Phone];
+            $peopleList[]=new People($id_card,$Name,$lastname,$Address,$county,$Province,$Phone);
+
+        }
+        require("connection_close.php");
+        return $peopleList ;
+
+    }
+
+
+} 
+?>
