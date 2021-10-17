14dad25dd50d1b6fd6438e0f83dfe0bd44004051
KanyanutM <91516461+KanyanutM@users.noreply.github.com>
2021-10-18 00:10:42 +0700

    Update conncetion_connect.php

--- conncetion_connect.php
+++ conncetion_connect.php
@@ -5,7 +5,7 @@
     $dbname = "db21_120";
 
     $conn = new mysqli($servername,$username,$password);
-    mysqli_set_charset($conn, "utf8");
+    mysqli_set_charset($conn, "utf8mb4");
     if($conn->connect_error){
         die("Connection failed:".$conn->connect_error);
     }
