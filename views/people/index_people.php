0a7f9387a98d9c97c5bc378d6d400155424044bd
KanyanutM <91516461+KanyanutM@users.noreply.github.com>
2021-10-17 23:49:59 +0700

    Update index_people.php

--- views/people/index_people.php
+++ views/people/index_people.php
@@ -1,24 +1,42 @@
+<?php
+    echo "กัญญาณัฐ ตั้งเจริญ 6220502060"
+?>
+<br>
+
 <table border = 1>
 
-<tr>
-    <td>id_card</td>
-    <td>Name</td>
-    <td>lastname</td>
-    <td>Address</td>
-    <td>county</td>
-    <td>Province</td>
-    <td>Phone</td>
-</tr>
-<?php foreach($people_list as $people)
-{
-    echo "<tr><td>$people->id_card</td>
-    <td>$people->Name</td>
-    <td>$people->lastname</td>
-    <td>$people->Address</td>
-    <td>$people->county</td>
-    <td>$people->Province</td>
-    <td>$people->Phone</td>" ;
-}    
 
-echo "</table>";
+    new quotation <a href="?controller=people&action=newPeople">click</a><br>
+
+    <form method="get"action="">
+        <input type="text" name="key">
+        <input type="hidden" name="controller" value="people"/>
+        <button type="submit" name="action" value="search">
+    search</button>
+</form>
+
+    <tr>
+        <td>id_card</td>
+        <td>Name</td>
+        <td>lastname</td>
+        <td>Address</td>
+        <td>county</td>
+        <td>Province</td>
+        <td>Phone</td>
+    </tr>
+    
+ <?php foreach($people_list as $people)
+ {
+     echo "<tr><td>$people->id_card</td>
+     <td>$people->Name</td>
+     <td>$people->lastname</td>
+     <td>$people->Address</td>
+     <td>$people->county</td>
+     <td>$people->Province</td>
+     <td>$people->Phone</td>
+     <td><a href=?controller=people&action=updateForm&id_card=$people->id_card>update</a></td>
+     <td><a href=?controller=people&action=deleteConfirm&id_card=$people->card>delete</a> </td> </tr>" ;
+ }    
+
+ echo "</table>";
  ?>
