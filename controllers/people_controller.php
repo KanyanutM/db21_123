67d5c3d31127fe6faffe79301ac6ed231feff3e3
KanyanutM <91516461+KanyanutM@users.noreply.github.com>
2021-10-19 00:46:33 +0700

    Update people_controller.php

--- controllers/people_controller.php
+++ controllers/people_controller.php
@@ -14,6 +14,8 @@
 
     public function addPeople()
     {
+        
+        //echo "111111";
         $id_card = $_GET['id_card'];
         $Name = $_GET['Name'];
         $lastname = $_GET['lastname'];
@@ -21,10 +23,11 @@
         $county = $_GET['county'];
         $Province = $_GET['Province'];
         $Phone = $_GET['Phone'];
-        //echo $QID ; 
-        $add = People::Add($id_card,$Name,$lastname,$Address,$county,$Province,$Phone);
+        //echo $id_card,$Name,$lastname,$Address,$county,$Province,$Phone; 
+        
+        People::Add($id_card,$Name,$lastname,$Address,$county,$Province,$Phone) ;
         //echo $add ;
-        PeopleController::index();
+        PeopleController::index() ;
     }
 
     public function search()
@@ -34,5 +37,26 @@
         require_once("./views/people/index_people.php");
     }
     
+    public function updateForm() 
+    {
+        $id_card = $_GET['id_card'];
+        $people = People::get($id_card);
+        require_once("./views/people/updateForm.php");
+    }
+
+    public function update()
+    {
+        $id_card = $_GET['id_card'];
+        $Name = $_GET['Name'];
+        $lastname = $_GET['lastname'];
+        $Address = $_GET['Address'];
+        $county = $_GET['county'];
+        $Province = $_GET['Province'];
+        $Phone = $_GET['Phone'];
+        //echo $Payment_Type ; 
+        People::update($id_card,$Name,$lastname,$Address,$county,$Province,$Phone); 
+        PeopleController::index();
+    }
+
 }
 ?>
