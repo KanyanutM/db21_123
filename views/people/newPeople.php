cee4579112817e12215acef817823dc989f8e353
KanyanutM <91516461+KanyanutM@users.noreply.github.com>
2021-10-18 16:48:11 +0700

    Update newPeople.php

---
+++ views/people/newPeople.php
@@ -0,0 +1,14 @@
+<form method="get" action="">
+    
+    <label>id_card <input type="text" name="id_card" /> </label><br>
+    <label>Name <input type="text" name="Name" /> </label><br>
+    <label>lastname <input type="text" name="lastname" /> </label><br>
+    <label>Address <input type="text" name="Address" /> </label><br>
+    <label>county <input type="text" name="county" /> </label><br>
+    <label>Province <input type="text" name="Province" /> </label><br>
+    <label>Phone <input type="text" name="Phone" /> </label><br>
+    
+    <input type="hidden" name="controller" value="people"/>
+    <button type="submit" name="action" value="index">Back</button>
+    <button type="submit" name="action" value="addPeople">Save</button>
+</form>
