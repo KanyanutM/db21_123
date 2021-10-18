9f3ca283aa2e9c0d8b187dcd505b6a92fc9d1c6c
KanyanutM <91516461+KanyanutM@users.noreply.github.com>
2021-10-18 16:48:21 +0700

    Update people_controller.php

--- controllers/people_controller.php
+++ controllers/people_controller.php
@@ -5,5 +5,34 @@
         $people_list = People::getAll();
         require_once("./views/people/index_people.php");
     }
+
+    public function newPeople()
+    {
+    
+        require_once("./views/people/newPeople.php");
+    }
+
+    public function addPeople()
+    {
+        $id_card = $_GET['id_card'];
+        $Name = $_GET['Name'];
+        $lastname = $_GET['lastname'];
+        $Address = $_GET['Address'];
+        $county = $_GET['county'];
+        $Province = $_GET['Province'];
+        $Phone = $_GET['Phone'];
+        //echo $QID ; 
+        $add = People::Add($id_card,$Name,$lastname,$Address,$county,$Province,$Phone);
+        //echo $add ;
+        PeopleController::index();
+    }
+
+    public function search()
+    {
+        $key = $_GET['key'];
+        $people_list = People::search($key);
+        require_once("./views/people/index_people.php");
+    }
+    
 }
 ?>
