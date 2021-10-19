e57c51062abcdd77888afdd6ca4b2c6274cf04f0
KanyanutM <91516461+KanyanutM@users.noreply.github.com>
2021-10-20 00:40:23 +0700

    Update booking.php

--- model/booking.php
+++ model/booking.php
@@ -16,6 +16,21 @@ class Booking{
     }
 
     public static function getAll(){
-        
+        $bookingList=[];
+        require("connection_connect.php");
+        $sql ="SELECT * FROM Booking " ;
+        $result=$conn->query($sql);
+        while($my_row=$result->fetch_assoc())
+        {
+            $id_b = $my_row[id_b];
+            $date_b = $my_row[date_b];
+            $time_b = $my_row[time_b];
+            $id_card =$my_row[id_card];
+            $id_checkpoint = $my_row[id_checkpoint];
+            $pricedetailList[]= new price_detail($id_b,$date_b,$time_b,$id_card,$id_checkpoint);
+        }
+
+        require("connection_close.php");
+        return $bookingList ;
     }
 }
