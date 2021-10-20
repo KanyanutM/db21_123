d803d2d6f0fa033c3d7d31de377d83dcb990d271
KanyanutM <91516461+KanyanutM@users.noreply.github.com>
2021-10-20 23:08:03 +0700

    Update CheckPoint.php

--- model/CheckPoint.php
+++ model/CheckPoint.php
@@ -1,20 +1,21 @@
 <?php class CheckPoint{
-    public $id_card;  
+    public $id_card; 
+    public $NameCheckPoint; 
     
-    public function __construct($id_card){
+    public function __construct($id_card,$NameCheckPoint){
         $this->id_card = $id_card;
-        
+        $this->NameCheckPoint = $NameCheckPoint;
     }
     public static function getAll(){
         $CheckPointList=[];
         require("connection_connect.php");
-        $sql ="SELECT CheckPoint.id FROM CheckPoint" ;
+        $sql ="SELECT CheckPoint.id ,CheckPoint.name FROM CheckPoint" ;
         $result=$conn->query($sql);
         while($my_row=$result->fetch_assoc())
         {
             $id_card = $my_row[id];
-
-            $CheckPointList[]= new CheckPoint($id_card);
+            $NameCheckPoint = $my_row[name];
+            $CheckPointList[]= new CheckPoint($id_card,$NameCheckPoint);
         }
     
         require("connection_close.php");
