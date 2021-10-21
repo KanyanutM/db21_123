9adebb4bf4f9f604da6c127ad3f96918d44095c5
KanyanutM <91516461+KanyanutM@users.noreply.github.com>
2021-10-22 00:24:23 +0700

    Update deleteConfirm.php

--- views/people/deleteConfirm.php
+++ views/people/deleteConfirm.php
@@ -2,7 +2,7 @@
             <br> $people->id_card $people->Name $people->lastname $people->Address $people->county $people->Province $people->Phone <br>";?>
 <form method="get" action="">
     <input type="hidden" name="controller" value="people"/>
-    <input type="hidden" name="QID" value="<?php echo $people->id_card;?>" />
+    <input type="hidden" name="id_card" value="<?php echo $people->id_card;?>" />
     <button type="submit" name="action" value="index">Back</button>
     <button type="submit" name="action" value="delete">delete</button>
 </form>
