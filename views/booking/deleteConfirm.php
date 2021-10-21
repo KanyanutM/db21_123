216a2d1d88d94c953ece26027b314d13f172efd5
KanyanutM <91516461+KanyanutM@users.noreply.github.com>
2021-10-22 00:24:14 +0700

    Create deleteConfirm.php

---
+++ views/booking/deleteConfirm.php
@@ -0,0 +1,8 @@
+<?php echo "<br>Are you sure to delete this Booking? <br>
+            <br> $booking->id_b $booking->data_b $booking->time_b $booking->id_card $booking->Name_b $booking->Name_checkpoint <br>";?>
+<form method="get" action="">
+    <input type="hidden" name="controller" value="booking"/>
+    <input type="hidden" name="id_b" value="<?php echo $booking->id_b;?>" />
+    <button type="submit" name="action" value="index">Back</button>
+    <button type="submit" name="action" value="delete">delete</button>
+</form>
