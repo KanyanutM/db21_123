0319a1a895ca5a6b4e6f1482f918ee2cbbfa2088
KanyanutM <91516461+KanyanutM@users.noreply.github.com>
2021-11-10 20:04:32 +0700

    Update deleteConfirm.php

---
+++ views/ATKtest/deleteConfirm.php
@@ -0,0 +1,8 @@
+<?php echo "<br>Are you sure to delete this ATKtest? <br>
+            <br> $atktest->id_atk $atktse->date_atk $atktest->time_atk $atkteat->results $atktest->id_b $atktest->id_card $atktest->Nameatktest $atktest->LastnameP $atktest->Name_checkpoint <br>";?>
+<form method="get" action="">
+    <input type="hidden" name="controller" value="atktest"/>
+    <input type="hidden" name="id_atk" value="<?php echo $atktest->id_atk;?>" />
+    <button type="submit" name="action" value="index">Back</button>
+    <button type="submit" name="action" value="delete">delete</button>
+</form>
