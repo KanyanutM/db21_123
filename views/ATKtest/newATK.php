8b7e233dd7ecd6114c5f6a05d5ad4c4585c6e829
KanyanutM <91516461+KanyanutM@users.noreply.github.com>
2021-11-11 02:57:29 +0700

    Update newATK.php

--- views/ATKtest/newATK.php
+++ views/ATKtest/newATK.php
@@ -3,13 +3,13 @@
     <label>id_atk <input type="text" name="id_atk" /> </label><br>
     <label>date_atk <input type="date" name="date_atk" /> </label><br>
     <label>time_atk <input type="time" name="time_atk" /> </label><br>
-    <label>resulte <input type="text" name="resulte" /> </label><br>
+    <label>results <input type="text" name="results" /> </label><br>
     
     <label>id_b <select name="id_b">
         <?php foreach($booking_list as $booking){echo "<option value= $booking->id_b>$booking->id_b </option>";}?></select></label><br>
 
     <label>id_staff <select name="id_staff">
-        <?php foreach($staff_list as $staff){echo "<option value= $staff->id_staff>$staff->id_staff </option>";}?></select></label><br>
+        <?php foreach($staffdetail_list as $staff){echo "<option value= $staff->id_staff_checkpoint>$staff->id_staff_checkpoint </option>";}?></select></label><br>
     
     
     <input type="hidden" name="controller" value="ATKtest"/>
