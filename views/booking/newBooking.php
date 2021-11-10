567b494f66a3057ffd85dc1db9cab93e8d25a394
KanyanutM <91516461+KanyanutM@users.noreply.github.com>
2021-11-10 20:04:39 +0700

    Update newBooking.php

--- views/booking/newBooking.php
+++ views/booking/newBooking.php
@@ -1,13 +1,12 @@
 <form method="get" action="">
     
     <label>id_b <input type="text" name="id_b" /> </label><br>
-    <label>date_b <input type="text" name="date_b" /> </label><br>
-    <label>time_b <input type="text" name="time_b" /> </label><br>
-    <label>id_card <input type="text" name="id_card" /> </label><br>
-    <label>NamePeople <select name="NamePeople">
-        <?php foreach($people_list as $NamePeople){echo "<option value= $NamePeople->id_card>$NamePeople->NamePeople</option>";}?></select></label><br>
-    <label>LastnameP <select name="LastnameP">
-        <?php foreach($people_list as $LastnameP){echo "<option value= $LastnameP->id_card>$LastnameP->LastnameP</option>";}?></select></label><br>
+    <label>date_b <input type="date" name="date_b" /> </label><br>
+    <label>time_b <input type="time" name="time_b" /> </label><br>
+    
+    <label>id_card <select name="id_card">
+        <?php foreach($people_list as $people){echo "<option value= $people->id_card>$people->id_card </option>";}?></select></label><br>
+   
     <label>Name_checkpoint <select name="Name_checkpoint">
         <?php foreach($checkpoint_list as $Name_checkpoint){echo "<option value= $Name_checkpoint->id_cp>$Name_checkpoint->Name_checkpoint</option>";}?></select></label><br>
     
