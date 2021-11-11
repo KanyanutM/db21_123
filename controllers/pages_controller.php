4b47d447f34cd956df6b8c40befb1f60ff8dc469
KanyanutM <91516461+KanyanutM@users.noreply.github.com>
2021-11-11 12:19:47 +0700

    Update pages_controller.php

--- controllers/pages_controller.php
+++ controllers/pages_controller.php
@@ -1,9 +1,17 @@
 <?php
-class PageController
-{
-    public function home()
-    { require_once('views/Page/home.php');}
-    public function error()
-    { require_once("views/Page/error.php");}
-}
+    class PageController
+    {
+        echo "55555";
+        public function home()
+        { 
+            echo "55555555555";
+            $sum_list = Sum::getAll();
+            //$fieldhospital_list = field_hospital::getsum($summarize_list) ;
+            require_once("./views/Pages/Home.php");
+        }
+
+        
+        public function error()
+        { require_once("./views/Pages/Error.php");}
+    }
 ?>
