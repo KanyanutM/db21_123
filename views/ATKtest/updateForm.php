30a438a0329b8a7b4f68a0d717f5f72efa820f21
KanyanutM <91516461+KanyanutM@users.noreply.github.com>
2021-11-11 12:47:45 +0700

    Update updateForm.php

--- views/ATKtest/updateForm.php
+++ views/ATKtest/updateForm.php
@@ -1,13 +1,13 @@
 <form method ="get" action="">
-    <label>id_atk <input type="text" name="newid"
+    <label>หมายเลขการตรวจ <input type="text" name="newid"
              value="<?php echo $atktest->id_atk; ?>" /> </label><br>
-    <label>date_atk <input type="date" name="new_date"
+    <label>วันที่ตรวจ <input type="date" name="new_date"
               value="<?php echo $atktest->date_atk; ?>" /> </label><br>
-    <label>time_atk <input type="time" name="new_time"
+    <label>เวลาที่ตรวจ <input type="time" name="new_time"
               value="<?php echo $atktest->time_atk; ?>" /> </label><br>
-    <label>results <input type="text" name="new_results"
+    <label>ผลการตรวจ <input type="text" name="new_results"
               value="<?php echo $atktest->results; ?>" /> </label><br>
-    <label>id_b <select name="new_idb">
+    <label>หมายเลขการจอง <select name="new_idb">
        <?php foreach($booking_list as $booking){
            echo "<option value= $booking->id_b";
          if($booking->id_b==$atktest->id_b){
@@ -16,7 +16,7 @@
             echo ">$booking->id_b</option>";
           }?></select></label><br>
     
-    <label>id_staff <select name="new_ids">
+    <label>หมายเลขผู้ตรวจ <select name="new_ids">
        <?php foreach($staffdetail_list as $staffdetail){
          echo "<option value= $staffdetail->id_staff_checkpoint";
          if($staffdetail->id_staff_checkpoint==$atktest->id_staff_checkpoint){
