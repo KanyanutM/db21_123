f9ce0ccb804d9c3f368cbdfa9f16b5518dd4d472
KanyanutM <91516461+KanyanutM@users.noreply.github.com>
2021-11-11 12:47:56 +0700

    Update Home.php

--- views/Pages/Home.php
+++ views/Pages/Home.php
@@ -1,1 +1,21 @@
 <p>Welcome to our homepage </p>
+
+<p>จำนวนในแต่ละจุด</p>
+<table border = 1>
+    
+    <tr>
+        <td><b>จุดตรวจ</b></td>
+        <td><b>จำนวนคนจอง</b></td>
+        <td><b>จำนวนคนติด</b></td>
+        
+    </tr>
+    <br>
+ <?php foreach($sum_list as $sum)
+ {
+     echo "<tr><td>$sum->name_checkpoint</td>
+     <td>$sum->sum</td>
+     <td>$sum->sum_result</td>
+     </tr> ";
+ }
+ echo "</table>";
+ ?>
