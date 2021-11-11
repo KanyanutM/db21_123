7ffe41a138d3bc7d1eb7c6c24391d31f2ec5be2c
KanyanutM <91516461+KanyanutM@users.noreply.github.com>
2021-11-11 12:47:48 +0700

    Update newBooking.php

--- views/booking/newBooking.php
+++ views/booking/newBooking.php
@@ -1,13 +1,13 @@
 <form method="get" action="">
     
-    <label>id_b <input type="text" name="id_b" /> </label><br>
-    <label>date_b <input type="date" name="date_b" /> </label><br>
-    <label>time_b <input type="time" name="time_b" /> </label><br>
+    <label>หมายเลขการจอง <input type="text" name="id_b" /> </label><br>
+    <label>วันที่จอง <input type="date" name="date_b" /> </label><br>
+    <label>เวลาจอง <input type="time" name="time_b" /> </label><br>
     
-    <label>id_card <select name="id_card">
+    <label>หมายเลขบัตรประชาชน <select name="id_card">
         <?php foreach($people_list as $people){echo "<option value= $people->id_card>$people->id_card </option>";}?></select></label><br>
    
-    <label>Name_checkpoint <select name="Name_checkpoint">
+    <label>ชื่อจุดตรวจ <select name="Name_checkpoint">
         <?php foreach($checkpoint_list as $Name_checkpoint){echo "<option value= $Name_checkpoint->id_cp>$Name_checkpoint->Name_checkpoint</option>";}?></select></label><br>
     
     
